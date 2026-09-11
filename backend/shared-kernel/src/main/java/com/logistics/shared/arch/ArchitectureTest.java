eb56b47231e45cf642fae55f7d760310e9f5848e
manish1710-007 <manishnik1710@gmail.com>
2026-09-11 20:07:41 +0530

    backend error fix

--- backend/shared-kernel/src/main/java/com/logistics/shared/arch/ArchitectureTest.java
+++ backend/shared-kernel/src/main/java/com/logistics/shared/arch/ArchitectureTest.java
@@ -5,17 +5,15 @@ import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
 
-@AnalyzeClasses(package = "com.logistics")
+@AnalyzeClasses(packages = "com.logistics")
 public class ArchitectureTest {
 
-    // This ensures modules do not depend on each pther diretly!
     @ArchTest
-    static final ArchRule moduleShouldNotDependOnEachOther =
+    static final ArchRule modulesShouldNotDependOnEachOther =
             slices().matching("com.logistics.(*)..")
                     .should().notDependOnEachOther()
                     .ignoreDependency(
-                        //Allow ignoring dependencies within the same module
-                        (origin, target) -> origin.getOwner().getName().startWith(target.getowner().getName())
+                        // Ignore the dependency if the target is the shared-kernel
+                        (origin, target) -> target.getPackageName().startsWith("com.logistics.shared")
                     );
-    
 }
